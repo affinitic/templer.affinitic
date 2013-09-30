afada84d8f91905b286a22fd02c5c2ef267334d2
schminitz <gillian@affinitic.be>
2013-09-30 11:43:53 +0200

    Diazo inherit for basebuildouttemplate refs #5396

--- src/templer/affinitic/diazo_theme.py
+++ src/templer/affinitic/diazo_theme.py
@@ -6,13 +6,13 @@ Licensed under the GPL license, see LICENCE.txt for more details.
 Copyright by Affinitic sprl
 """
 
-from templer.affinitic.base import AffiniticBaseTemplate
+from templer.affinitic.base import AffiniticBaseBuildoutTemplate
 
 
-class DiazoTheme(AffiniticBaseTemplate):
+class DiazoTheme(AffiniticBaseBuildoutTemplate):
     _template_dir = 'templates/diazo_theme'
     summary = "A basic Diazo theme package skeleton"
-    help = "This creates a basic skeleton for a Plone Diazo theme."
+    help = "This creates a basic skeleton for a Plone Buildout Diazo theme."
     category = "Affinitic"
     required_templates = []
     default_required_structures = ['bootstrap', ]
